Bakkhos/aepp-sdk-python

commit d97894ea689ffeebe14c2628adbbd15610b73e4f

Chore/general cleanup and fixes (#108)
* Set minimum required python version to 3.6
* Fix cli test, set the nonce for offline mode
* Set operations default to native, change parameter name to --debug-tx to use debug tx generation
* Remove legacy aevm submodule
* Move constant identifiers to identifier submodule and use the constants around the codebase
* Better default values for tx ttl, contract deposit and amount, oracle query fee
* Use namedtuple to parse node responses instead of mix namedtuple and dict

aeternity/transactions.py
@@ -1,63 +1,7 @@
 from aeternity.hashing import _int, _binary, _id, encode, decode, encode_rlp, hash_encode, contract_id
 from aeternity.openapi import OpenAPICli
 from aeternity.config import ORACLE_DEFAULT_TTL_TYPE_DELTA
-
-# RLP version number
-# https://github.com/aeternity/protocol/blob/api-v0.10.1/serializations.md#binary-serialization
-VSN = 1
-
-# Tag constant for ids (type uint8)
-# see https://github.com/aeternity/protocol/blob/master/serializations.md#the-id-type
-# <<Tag:1/unsigned-integer-unit:8, Hash:32/binary-unit:8>>
-ID_TAG_ACCOUNT = 1
-ID_TAG_NAME = 2
-ID_TAG_COMMITMENT = 3
-ID_TAG_ORACLE = 4
-ID_TAG_CONTRACT = 5
-ID_TAG_CHANNEL = 6
-
-# Object tags
-# see https://github.com/aeternity/protocol/blob/master/serializations.md#binary-serialization
-
-OBJECT_TAG_ACCOUNT = 10
-OBJECT_TAG_SIGNED_TRANSACTION = 11
-OBJECT_TAG_SPEND_TRANSACTION = 12
-OBJECT_TAG_ORACLE = 20
-OBJECT_TAG_ORACLE_QUERY = 21
-OBJECT_TAG_ORACLE_REGISTER_TRANSACTION = 22
-OBJECT_TAG_ORACLE_QUERY_TRANSACTION = 23
-OBJECT_TAG_ORACLE_RESPONSE_TRANSACTION = 24
-OBJECT_TAG_ORACLE_EXTEND_TRANSACTION = 25
-OBJECT_TAG_NAME_SERVICE_NAME = 30
-OBJECT_TAG_NAME_SERVICE_COMMITMENT = 31
-OBJECT_TAG_NAME_SERVICE_CLAIM_TRANSACTION = 32
-OBJECT_TAG_NAME_SERVICE_PRECLAIM_TRANSACTION = 33
-OBJECT_TAG_NAME_SERVICE_UPDATE_TRANSACTION = 34
-OBJECT_TAG_NAME_SERVICE_REVOKE_TRANSACTION = 35
-OBJECT_TAG_NAME_SERVICE_TRANSFER_TRANSACTION = 36
-OBJECT_TAG_CONTRACT = 40
-OBJECT_TAG_CONTRACT_CALL = 41
-OBJECT_TAG_CONTRACT_CREATE_TRANSACTION = 42
-OBJECT_TAG_CONTRACT_CALL_TRANSACTION = 43
-OBJECT_TAG_CHANNEL_CREATE_TRANSACTION = 50
-OBJECT_TAG_CHANNEL_DEPOSIT_TRANSACTION = 51
-OBJECT_TAG_CHANNEL_WITHDRAW_TRANSACTION = 52
-OBJECT_TAG_CHANNEL_FORCE_PROGRESS_TRANSACTION = 521
-OBJECT_TAG_CHANNEL_CLOSE_MUTUAL_TRANSACTION = 53
-OBJECT_TAG_CHANNEL_CLOSE_SOLO_TRANSACTION = 54
-OBJECT_TAG_CHANNEL_SLASH_TRANSACTION = 55
-OBJECT_TAG_CHANNEL_SETTLE_TRANSACTION = 56
-OBJECT_TAG_CHANNEL_OFF_CHAIN_TRANSACTION = 57
-OBJECT_TAG_CHANNEL_OFF_CHAIN_UPDATE_TRANSFER = 570
-OBJECT_TAG_CHANNEL_OFF_CHAIN_UPDATE_DEPOSIT = 571
-OBJECT_TAG_CHANNEL_OFF_CHAIN_UPDATE_WITHDRAWAL = 572
-OBJECT_TAG_CHANNEL_OFF_CHAIN_UPDATE_CREATE_CONTRACT = 573
-OBJECT_TAG_CHANNEL_OFF_CHAIN_UPDATE_CALL_CONTRACT = 574
-OBJECT_TAG_CHANNEL = 58
-OBJECT_TAG_CHANNEL_SNAPSHOT_TRANSACTION = 59
-OBJECT_TAG_POI = 60
-OBJECT_TAG_MICRO_BODY = 101
-OBJECT_TAG_LIGHT_MICRO_BLOCK = 102
+from aeternity import identifiers as idf
 
 
 class TxSigner:
@@ -71,10 +15,10 @@ class TxSigner:
 
     def encode_signed_transaction(self, transaction, signature):
         """prepare a signed transaction message"""
-        tag = bytes([OBJECT_TAG_SIGNED_TRANSACTION])
-        vsn = bytes([VSN])
-        encoded_signed_tx = encode_rlp("tx", [tag, vsn, [signature], transaction])
-        encoded_signature = encode("sg", signature)
+        tag = bytes([idf.OBJECT_TAG_SIGNED_TRANSACTION])
+        vsn = bytes([idf.VSN])
+        encoded_signed_tx = encode_rlp(idf.TRANSACTION, [tag, vsn, [signature], transaction])
+        encoded_signature = encode(idf.SIGNATURE, signature)
         return encoded_signed_tx, encoded_signature
 
     def sign_encode_transaction(self, tx):
@@ -83,7 +27,7 @@ class TxSigner:
         :return: encoded_signed_tx, encoded_signature, tx_hash
         """
         # decode the transaction if not in native mode
-        transaction = decode(tx.tx) if hasattr(tx, "tx") else decode(tx)
+        transaction = decode(tx.tx) if hasattr(tx, idf.TRANSACTION) else decode(tx)
         # sign the transaction
         signature = self.account.sign(_binary(self.network_id) + transaction)
         # encode the transaction
@@ -115,7 +59,7 @@ class TxBuilder:
         :param signed_tx: an encoded signed transaction
         """
         signed = decode(signed_tx)
-        return hash_encode("th", signed)
+        return hash_encode(idf.TRANSACTION_HASH, signed)
 
     def tx_spend(self, account_id, recipient_id, amount, payload, fee, ttl, nonce)-> str:
         """
@@ -131,17 +75,17 @@ class TxBuilder:
         # compute the absolute ttl and the nonce
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_SPEND_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ACCOUNT, account_id),
-                _id(ID_TAG_ACCOUNT, recipient_id),
+                _int(idf.OBJECT_TAG_SPEND_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ACCOUNT, account_id),
+                _id(idf.ID_TAG_ACCOUNT, recipient_id),
                 _int(amount),
                 _int(fee),
                 _int(ttl),
                 _int(nonce),
                 _binary(payload)
             ]
-            tx = encode_rlp("tx", tx)
+            tx = encode_rlp(idf.TRANSACTION, tx)
             return tx
 
         # use internal endpoints transaction
@@ -169,15 +113,15 @@ class TxBuilder:
         """
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_NAME_SERVICE_PRECLAIM_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ACCOUNT, account_id),
+                _int(idf.OBJECT_TAG_NAME_SERVICE_PRECLAIM_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ACCOUNT, account_id),
                 _int(nonce),
-                _id(ID_TAG_COMMITMENT, commitment_id),
+                _id(idf.ID_TAG_COMMITMENT, commitment_id),
                 _int(fee),
                 _int(ttl)
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             commitment_id=commitment_id,
@@ -200,16 +144,16 @@ class TxBuilder:
         """
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_NAME_SERVICE_CLAIM_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ACCOUNT, account_id),
+                _int(idf.OBJECT_TAG_NAME_SERVICE_CLAIM_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ACCOUNT, account_id),
                 _int(nonce),
                 decode(name),
                 _binary(name_salt),
                 _int(fee),
                 _int(ttl)
             ]
-            tx = encode_rlp("tx", tx)
+            tx = encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             account_id=account_id,
@@ -237,26 +181,26 @@ class TxBuilder:
             # TODO: verify supported keys for name updates
             def pointer_tag(pointer):
                 return {
-                    "account_pubkey": ID_TAG_ACCOUNT,
-                    "oracle_pubkey": ID_TAG_ORACLE,
-                    "contract_pubkey": ID_TAG_CONTRACT,
-                    "channel_pubkey": ID_TAG_CHANNEL
+                    "account_pubkey": idf.ID_TAG_ACCOUNT,
+                    "oracle_pubkey": idf.ID_TAG_ORACLE,
+                    "contract_pubkey": idf.ID_TAG_CONTRACT,
+                    "channel_pubkey": idf.ID_TAG_CHANNEL
                 }.get(pointer.get("key"))
-            ptrs = [[_binary(p.get("key")), _id(pointer_tag(p), p.get("id"))] for p in pointers]
+            ptrs = [[_binary(p.get("key")), _id(idf.pointer_tag(p), p.get("id"))] for p in pointers]
             # build tx
             tx = [
-                _int(OBJECT_TAG_NAME_SERVICE_UPDATE_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ACCOUNT, account_id),
+                _int(idf.OBJECT_TAG_NAME_SERVICE_UPDATE_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ACCOUNT, account_id),
                 _int(nonce),
-                _id(ID_TAG_NAME, name_id),
+                _id(idf.ID_TAG_NAME, name_id),
                 _int(name_ttl),
                 ptrs,
                 _int(client_ttl),
                 _int(fee),
                 _int(ttl)
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             account_id=account_id,
@@ -282,16 +226,16 @@ class TxBuilder:
         """
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_NAME_SERVICE_TRANSFER_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ACCOUNT, account_id),
+                _int(idf.OBJECT_TAG_NAME_SERVICE_TRANSFER_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ACCOUNT, account_id),
                 _int(nonce),
-                _id(ID_TAG_NAME, name_id),
-                _id(ID_TAG_ACCOUNT, recipient_id),
+                _id(idf.ID_TAG_NAME, name_id),
+                _id(idf.ID_TAG_ACCOUNT, recipient_id),
                 _int(fee),
                 _int(ttl),
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             account_id=account_id,
@@ -315,15 +259,15 @@ class TxBuilder:
 
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_NAME_SERVICE_REVOKE_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ACCOUNT, account_id),
+                _int(idf.OBJECT_TAG_NAME_SERVICE_REVOKE_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ACCOUNT, account_id),
                 _int(nonce),
-                _id(ID_TAG_NAME, name_id),
+                _id(idf.ID_TAG_NAME, name_id),
                 _int(fee),
                 _int(ttl),
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             account_id=account_id,
@@ -354,7 +298,7 @@ class TxBuilder:
 
         if self.native_transactions:
             tx = [
-                _id(account_id),
+                _id(idf.account_id),
                 _int(nonce),
                 _binary(code),
                 _int(vm_version),
@@ -366,7 +310,7 @@ class TxBuilder:
                 _int(gas_price),
                 _binary(call_data),
             ]
-            return encode_rlp("tx", tx), contract_id(account_id, nonce)
+            return encode_rlp(idf.TRANSACTION, tx), contract_id(idf.account_id, nonce)
         # use internal endpoints transaction
         body = dict(
             owner_id=account_id,
@@ -402,9 +346,9 @@ class TxBuilder:
         """
         if self.native_transactions:
             tx = [
-                _id(account_id),
+                _id(idf.account_id),
                 _int(nonce),
-                _id(contract_id),
+                _id(idf.contract_id),
                 _int(vm_version),
                 _int(fee),
                 _int(ttl),
@@ -413,7 +357,7 @@ class TxBuilder:
                 _int(gas_price),
                 _binary(call_data),
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             call_data=call_data,
@@ -441,9 +385,9 @@ class TxBuilder:
 
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_ORACLE_REGISTER_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ACCOUNT, account_id),
+                _int(idf.OBJECT_TAG_ORACLE_REGISTER_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ACCOUNT, account_id),
                 _int(nonce),
                 _binary(query_format),
                 _binary(response_format),
@@ -454,7 +398,7 @@ class TxBuilder:
                 _int(ttl),
                 _int(vm_version),
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             account_id=account_id,
@@ -482,11 +426,11 @@ class TxBuilder:
 
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_ORACLE_QUERY_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ACCOUNT, sender_id),
+                _int(idf.OBJECT_TAG_ORACLE_QUERY_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ACCOUNT, sender_id),
                 _int(nonce),
-                _id(ID_TAG_ORACLE, oracle_id),
+                _id(idf.ID_TAG_ORACLE, oracle_id),
                 _binary(query),
                 _int(query_fee),
                 _int(0 if query_ttl_type == ORACLE_DEFAULT_TTL_TYPE_DELTA else 1),
@@ -496,7 +440,7 @@ class TxBuilder:
                 _int(fee),
                 _int(ttl),
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             sender_id=sender_id,
@@ -527,9 +471,9 @@ class TxBuilder:
 
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_ORACLE_RESPONSE_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ORACLE, oracle_id),
+                _int(idf.OBJECT_TAG_ORACLE_RESPONSE_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ORACLE, oracle_id),
                 _int(nonce),
                 _binary(query_id),
                 _binary(response),
@@ -538,7 +482,7 @@ class TxBuilder:
                 _int(fee),
                 _int(ttl),
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             response_ttl=dict(
@@ -564,16 +508,16 @@ class TxBuilder:
 
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_ORACLE_EXTEND_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_ORACLE, oracle_id),
+                _int(idf.OBJECT_TAG_ORACLE_EXTEND_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_ORACLE, oracle_id),
                 _int(nonce),
                 _int(0 if ttl_type == ORACLE_DEFAULT_TTL_TYPE_DELTA else 1),
                 _int(ttl_value),
                 _int(fee),
                 _int(ttl),
             ]
-            return encode_rlp("tx", tx)
+            return encode_rlp(idf.TRANSACTION, tx)
         # use internal endpoints transaction
         body = dict(
             oracle_id=oracle_id,

.gitignore
@@ -7,3 +7,6 @@ venv
 aepp_sdk.egg-info
 dist
 build
+coverage.xml
+docker-compose.override.yml
+test-results.xml

aeternity/__main__.py
@@ -4,6 +4,7 @@ import os
 import json
 import sys
 import getpass
+import namedtupled
 
 from aeternity import __version__
 
@@ -130,7 +131,7 @@ def _print_object(data, title=None):
 
     if ctx.obj.get(CTX_OUTPUT_JSON, False):
         if isinstance(data, tuple):
-            print(json.dumps(data._asdict(), indent=2))
+            print(json.dumps(namedtupled.reduce(data), indent=2))
             return
         if isinstance(data, str):
             print(data)
@@ -160,8 +161,8 @@ _sign_options = [
 ]
 
 _transaction_options = [
-    click.option('--native', 'native', is_flag=True, default=False,
-                 help='Use native transaction generation instead of the internal endpoints (always True for offline transactions)'),
+    click.option('--debug-tx', 'debug_tx', is_flag=True, default=False,
+                 help='Use debug transaction generation endpoints from the node instead of the native python implementation'),
     click.option('--ttl', 'ttl', type=int, default=config.DEFAULT_TX_TTL,
                  help=f'Set the transaction ttl (relative number, ex 100)', show_default=True),
     click.option('--fee', 'fee', type=int, default=config.DEFAULT_FEE,
@@ -408,11 +409,11 @@ def tx_broadcast(signed_transaction, force, wait, json_):
 @click.argument('amount', required=True, type=int)
 @transaction_options
 @click.option('--payload', default="", help="Spend transaction payload")
-def tx_spend(sender_id, recipient_id, amount, native, ttl, fee, nonce, payload, force, wait, json_):
+def tx_spend(sender_id, recipient_id, amount, debug_tx, ttl, fee, nonce, payload, force, wait, json_):
     try:
         set_global_options(force, wait, json_)
-        cli = _node_cli(native=native)
-        if not native:
+        cli = _node_cli(native=not debug_tx)
+        if debug_tx:
             nonce, ttl = cli._get_nonce_ttl(sender_id, ttl)
         tx = cli.tx_builder.tx_spend(sender_id, recipient_id, amount, payload, fee, ttl, nonce)
         # print the results

aeternity/aens.py
@@ -2,6 +2,7 @@ from aeternity.exceptions import NameNotAvailable, MissingPreclaim, NameUpdateEr
 from aeternity.openapi import OpenAPIClientException
 from aeternity.config import DEFAULT_TX_TTL, DEFAULT_FEE, DEFAULT_NAME_TTL, NAME_CLIENT_TTL
 from aeternity import hashing, utils, oracles
+from aeternity.identifiers import ACCOUNT_ID, COMMITMENT, NAME
 
 
 class NameStatus:
@@ -49,18 +50,18 @@ class AEName:
         """
         if isinstance(name, str):
             name = name.encode('ascii')
-        return hashing.encode('nm', name)
+        return hashing.encode(NAME, name)
 
     def _get_commitment_id(self):
         """
         Compute the commitment id
         """
         self.preclaim_salt = hashing.randint()
-        commitment_id = hashing.hash_encode("cm", hashing.namehash(self.domain) + self.preclaim_salt.to_bytes(32, 'big'))
+        commitment_id = hashing.hash_encode(COMMITMENT, hashing.namehash(self.domain) + self.preclaim_salt.to_bytes(32, 'big'))
         return commitment_id
 
     def _get_pointers(self, target):
-        if target.startswith('ak'):
+        if target.startswith(ACCOUNT_ID):
             pointers = [{'id': target, 'key': 'account_pubkey'}]
         else:
             pointers = [{'id': target, 'key': 'oracle_pubkey'}]
@@ -196,7 +197,7 @@ class AEName:
                 raise ValueError('You must register the oracle before using it as target')
             target = target.oracle_id
         # get the name_id and pointers
-        name_id = hashing.namehash_encode("nm", self.domain)
+        name_id = hashing.namehash_encode(NAME, self.domain)
         pointers = self._get_pointers(target)
         # get the transaction builder
         txb = self.client.tx_builder
@@ -216,7 +217,7 @@ class AEName:
         :return: the transaction
         """
         # get the name_id and pointers
-        name_id = hashing.namehash_encode("nm", self.domain)
+        name_id = hashing.namehash_encode(NAME, self.domain)
         # get the transaction builder
         txb = self.client.tx_builder
         # get the account nonce and ttl
@@ -237,7 +238,7 @@ class AEName:
         :return: the transaction
         """
         # get the name_id and pointers
-        name_id = hashing.namehash_encode("nm", self.domain)
+        name_id = hashing.namehash_encode(NAME, self.domain)
         # get the transaction builder
         txb = self.client.tx_builder
         # get the account nonce and ttl

aeternity/aevm.py
@@ -1,163 +1,0 @@
-
-# AEVM opcodes
-#
-# as defined in /node/apps/aebytecode/include/aeb_opcodes.hrl
-#
-import binascii
-
-# based on https://github.com/holiman/evmlab/blob/master/evmlab/opcodes.py
-#
-# gas prices might differ for AE
-
-# schema: [opcode, ins, outs, gas, arguments]
-OPCODES = {
-    0x00: ('STOP', 0, 0, 0, 0),
-    0x01: ('ADD', 2, 1, 3, 0),
-    0x02: ('MUL', 2, 1, 5, 0),
-    0x03: ('SUB', 2, 1, 3, 0),
-    0x04: ('DIV', 2, 1, 5, 0),
-    0x05: ('SDIV', 2, 1, 5, 0),
-    0x06: ('MOD', 2, 1, 5, 0),
-    0x07: ('SMOD', 2, 1, 5, 0),
-    0x08: ('ADDMOD', 3, 1, 8, 0),
-    0x09: ('MULMOD', 3, 1, 8, 0),
-    0x0a: ('EXP', 2, 1, 10, 0),
-    0x0b: ('SIGNEXTEND', 2, 1, 5, 0),
-    0x10: ('LT', 2, 1, 3, 0),
-    0x11: ('GT', 2, 1, 3, 0),
-    0x12: ('SLT', 2, 1, 3, 0),
-    0x13: ('SGT', 2, 1, 3, 0),
-    0x14: ('EQ', 2, 1, 3, 0),
-    0x15: ('ISZERO', 1, 1, 3, 0),
-    0x16: ('AND', 2, 1, 3, 0),
-    0x17: ('OR', 2, 1, 3, 0),
-    0x18: ('XOR', 2, 1, 3, 0),
-    0x19: ('NOT', 1, 1, 3, 0),
-    0x1a: ('BYTE', 2, 1, 3, 0),
-    0x20: ('SHA3', 2, 1, 30, 0),
-    0x30: ('ADDRESS', 0, 1, 2, 0),
-    0x31: ('BALANCE', 1, 1, 20, 0),
-    0x32: ('ORIGIN', 0, 1, 2, 0),
-    0x33: ('CALLER', 0, 1, 2, 0),
-    0x34: ('CALLVALUE', 0, 1, 2, 0),
-    0x35: ('CALLDATALOAD', 1, 1, 3, 0),
-    0x36: ('CALLDATASIZE', 0, 1, 2, 0),
-    0x37: ('CALLDATACOPY', 3, 0, 3, 0),
-    0x38: ('CODESIZE', 0, 1, 2, 0),
-    0x39: ('CODECOPY', 3, 0, 3, 0),
-    0x3a: ('GASPRICE', 0, 1, 2, 0),
-    0x3b: ('EXTCODESIZE', 1, 1, 20, 0),
-    0x3c: ('EXTCODECOPY', 4, 0, 20, 0),
-    0x3d: ('RETURNDATASIZE', 0, 1, 2, 0),
-    0x3e: ('RETURNDATACOPY', 3, 0, 3, 0),
-    0x40: ('BLOCKHASH', 1, 1, 20, 0),
-    0x41: ('COINBASE', 0, 1, 2, 0),
-    0x42: ('TIMESTAMP', 0, 1, 2, 0),
-    0x43: ('NUMBER', 0, 1, 2, 0),
-    0x44: ('DIFFICULTY', 0, 1, 2, 0),
-    0x45: ('GASLIMIT', 0, 1, 2, 0),
-    0x50: ('POP', 1, 0, 2, 0),
-    0x51: ('MLOAD', 1, 1, 3, 0),
-    0x52: ('MSTORE', 2, 0, 3, 0),
-    0x53: ('MSTORE8', 2, 0, 3, 0),
-    0x54: ('SLOAD', 1, 1, 50, 0),
-    0x55: ('SSTORE', 2, 0, 0, 0),
-    0x56: ('JUMP', 1, 0, 8, 0),
-    0x57: ('JUMPI', 2, 0, 10, 0),
-    0x58: ('PC', 0, 1, 2, 0),
-    0x59: ('MSIZE', 0, 1, 2, 0),
-    0x5a: ('GAS', 0, 1, 2, 0),
-    0x5b: ('JUMPDEST', 0, 0, 1, 0),
-
-    0x60: ('PUSH1', 0, 0, 0, 1),
-    0x61: ('PUSH2', 0, 0, 0, 2),
-    0x62: ('PUSH3', 0, 0, 0, 3),
-    0x63: ('PUSH4', 0, 0, 0, 4),
-    0x64: ('PUSH5', 0, 0, 0, 5),
-    0x65: ('PUSH6', 0, 0, 0, 6),
-    0x66: ('PUSH7', 0, 0, 0, 7),
-    0x67: ('PUSH8', 0, 0, 0, 8),
-    0x68: ('PUSH9', 0, 0, 0, 9),
-    0x69: ('PUSH10', 0, 0, 0, 10),
-    0x6a: ('PUSH11', 0, 0, 0, 11),
-    0x6b: ('PUSH12', 0, 0, 0, 12),
-    0x6c: ('PUSH13', 0, 0, 0, 13),
-    0x6d: ('PUSH14', 0, 0, 0, 14),
-    0x6e: ('PUSH15', 0, 0, 0, 15),
-    0x6f: ('PUSH16', 0, 0, 0, 16),
-    0x70: ('PUSH17', 0, 0, 0, 17),
-    0x71: ('PUSH18', 0, 0, 0, 18),
-    0x72: ('PUSH19', 0, 0, 0, 19),
-    0x73: ('PUSH20', 0, 0, 0, 20),
-    0x74: ('PUSH21', 0, 0, 0, 21),
-    0x75: ('PUSH22', 0, 0, 0, 22),
-    0x76: ('PUSH23', 0, 0, 0, 23),
-    0x77: ('PUSH24', 0, 0, 0, 24),
-    0x78: ('PUSH25', 0, 0, 0, 25),
-    0x79: ('PUSH26', 0, 0, 0, 26),
-    0x7a: ('PUSH27', 0, 0, 0, 27),
-    0x7b: ('PUSH28', 0, 0, 0, 28),
-    0x7c: ('PUSH29', 0, 0, 0, 29),
-    0x7d: ('PUSH30', 0, 0, 0, 30),
-    0x7e: ('PUSH31', 0, 0, 0, 31),
-    0x7f: ('PUSH32', 0, 0, 0, 32),
-    0x80: ('DUP1', 0, 0, 0, 0),
-    0x81: ('DUP2', 0, 0, 0, 0),
-    0x82: ('DUP3', 0, 0, 0, 0),
-    0x83: ('DUP4', 0, 0, 0, 0),
-    0x84: ('DUP5', 0, 0, 0, 0),
-    0x85: ('DUP6', 0, 0, 0, 0),
-    0x86: ('DUP7', 0, 0, 0, 0),
-    0x87: ('DUP8', 0, 0, 0, 0),
-    0x88: ('DUP9', 0, 0, 0, 0),
-    0x89: ('DUP10', 0, 0, 0, 0),
-    0x8a: ('DUP11', 0, 0, 0, 0),
-    0x8b: ('DUP12', 0, 0, 0, 0),
-    0x8c: ('DUP13', 0, 0, 0, 0),
-    0x8d: ('DUP14', 0, 0, 0, 0),
-    0x8e: ('DUP15', 0, 0, 0, 0),
-    0x8f: ('DUP16', 0, 0, 0, 0),
-    0x90: ('SWAP1', 0, 0, 0, 0),
-    0x91: ('SWAP2', 0, 0, 0, 0),
-    0x92: ('SWAP3', 0, 0, 0, 0),
-    0x93: ('SWAP4', 0, 0, 0, 0),
-    0x94: ('SWAP5', 0, 0, 0, 0),
-    0x95: ('SWAP6', 0, 0, 0, 0),
-    0x96: ('SWAP7', 0, 0, 0, 0),
-    0x97: ('SWAP8', 0, 0, 0, 0),
-    0x98: ('SWAP9', 0, 0, 0, 0),
-
-    0xa0: ('LOG0', 2, 0, 375, 0),
-    0xa1: ('LOG1', 3, 0, 750, 0),
-    0xa2: ('LOG2', 4, 0, 1125, 0),
-    0xa3: ('LOG3', 5, 0, 1500, 0),
-    0xa4: ('LOG4', 6, 0, 1875, 0),
-    0xf0: ('CREATE', 3, 1, 32000, 0),
-    0xf1: ('CALL', 7, 1, 40, 0),
-    0xf2: ('CALLCODE', 7, 1, 40, 0),
-    0xf3: ('RETURN', 2, 0, 0, 0),
-    0xf4: ('DELEGATECALL', 6, 0, 40, 0),
-    0xfa: ('STATICCALL', 6, 1, 40, 0),
-    0xfd: ('REVERT', 2, 0, 0, 0),
-    0xff: ('SUICIDE', 1, 0, 0, 0),
-}
-
-
-def pretty_bytecode(bytecode_as_hex):
-    if bytecode_as_hex.startswith('0x'):
-        bytecode_as_hex = bytecode_as_hex[2:]
-    bytecode_as_bytes = binascii.unhexlify(bytecode_as_hex)
-
-    i = 0
-    while i < len(bytecode_as_bytes):
-        opcode = bytecode_as_bytes[i]
-        name, ins, outs, gas, consumes = OPCODES.get(opcode, (f'UNKNOWN ({opcode})!', 0, 0, 0))
-        arguments = bytecode_as_bytes[i + 1:i + 1 + consumes]
-        arguments_ascii = arguments.decode('ascii', errors='replace')
-        arguments_hex = binascii.hexlify(arguments).decode('ascii')
-        pos = '0x%04x' % i
-        if arguments:
-            print(pos, name, arguments_hex, f'"{arguments_ascii}"')
-        else:
-            print(pos, name)
-        i += 1 + consumes

commit 4e8b8d985a91854d5291aa5b7df81903f07461e5

Upstream changes in d97894ea689ffeebe14c2628adbbd15610b73e4f broke my functions in aeternity/transactions.py - fixed
    Details:
    - I had to substitute ID_TAG_X -> aeternity.transactions.ID_TAG_X; likewise for object tags and some other constants.
    - Fixed tx_contract_create and tx_contract_call

aeternity/transactions.py
@@ -49,8 +49,8 @@ class TxSigner:
         signature1 = self.account.sign(_binary(self.network_id) + transaction)
         signature2 = other_account.sign(_binary(self.network_id) + transaction)
         #encode
-        tag = bytes([OBJECT_TAG_SIGNED_TRANSACTION])
-        vsn = bytes([VSN])
+        tag = bytes([idf.OBJECT_TAG_SIGNED_TRANSACTION])
+        vsn = bytes([idf.VSN])
         encoded_cosigned_tx = encode_rlp("tx", [tag, vsn, sorted([signature1, signature2]), transaction])
 
         return (encoded_cosigned_tx,
@@ -94,10 +94,10 @@ class TxBuilder:
                 state = decode(state)
 
             tx = [
-                _int(OBJECT_TAG_CHANNEL_CLOSE_SOLO_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_CHANNEL, channel_id),
-                _id(ID_TAG_ACCOUNT, account_id), #from_id
+                _int(idf.OBJECT_TAG_CHANNEL_CLOSE_SOLO_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_CHANNEL, channel_id),
+                _id(idf.ID_TAG_ACCOUNT, account_id), #from_id
                 _binary(state), #payload
                 _binary(decode(poi)),
                 _int(ttl),
@@ -138,10 +138,10 @@ class TxBuilder:
 
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_CHANNEL_SETTLE_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_CHANNEL, channel_id),
-                _id(ID_TAG_ACCOUNT, account_id), #from_id
+                _int(idf.OBJECT_TAG_CHANNEL_SETTLE_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_CHANNEL, channel_id),
+                _id(idf.ID_TAG_ACCOUNT, account_id), #from_id
                 _int(initiator_amount_final),
                 _int(responder_amount_final),
                 _int(ttl),
@@ -171,10 +171,10 @@ class TxBuilder:
                                 nonce : int):
         if self.native_transactions:
             tx = [
-                _int(OBJECT_TAG_CHANNEL_CLOSE_MUTUAL_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_CHANNEL, channel_id),
-                _id(ID_TAG_ACCOUNT, account_id),
+                _int(idf.OBJECT_TAG_CHANNEL_CLOSE_MUTUAL_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_CHANNEL, channel_id),
+                _id(idf.ID_TAG_ACCOUNT, account_id),
                 _int(initiator_amount_final),
                 _int(responder_amount_final),
                 _int(ttl),
@@ -208,10 +208,10 @@ class TxBuilder:
                 state = decode(state)
 
             tx = [
-                _int(OBJECT_TAG_CHANNEL_SLASH_TRANSACTION),
-                _int(VSN),
-                _id(ID_TAG_CHANNEL, channel_id),
-                _id(ID_TAG_ACCOUNT, account_id), #from_id
+                _int(idf.OBJECT_TAG_CHANNEL_SLASH_TRANSACTION),
+                _int(idf.VSN),
+                _id(idf.ID_TAG_CHANNEL, channel_id),
+                _id(idf.ID_TAG_ACCOUNT, account_id), #from_id
                 _binary(state), #payload
                 _binary(decode(poi)),
                 _int(ttl),
@@ -251,10 +251,10 @@ class TxBuilder:
             if isinstance(state, str):
                 state = decode(state)
 
-            tx = [ _int(OBJECT_TAG_CHANNEL_FORCE_PROGRESS_TRANSACTION),
-                   _int(VSN),
-                   _id(ID_TAG_CHANNEL, channel_id),
-                   _id(ID_TAG_ACCOUNT, account_id), #from_id
+            tx = [ _int(idf.OBJECT_TAG_CHANNEL_FORCE_PROGRESS_TRANSACTION),
+                   _int(idf.VSN),
+                   _id(idf.ID_TAG_CHANNEL, channel_id),
+                   _id(idf.ID_TAG_ACCOUNT, account_id), #from_id
                    _binary(state), #payload
                    _int(new_round), #round
                    _binary(decode(contract_call_update)),